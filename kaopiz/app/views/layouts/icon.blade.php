bf5851ee0278b4b8fd61eb760222c41d49567584
tuanndph16237 <tuanndph16237@example.com>
2023-04-20 11:55:19 +0700

    remove

--- kaopiz/app/views/layouts/icon.blade.php
+++
@@ -1,2 +0,0 @@
-{{-- icon --}}
-<script src="{{BASE_URL.'public/js/lumino.glyphs.js'}}"></script>
--- kaopiz/storage/2abd3fa146d27a29d4dcca3ffc400c8af846a10c.php
+++
@@ -1,2 +0,0 @@
-
-<script src="<?php echo e(BASE_URL.'public/js/lumino.glyphs.js'); ?>"></script><?php /**PATH C:\xampp\htdocs\php2\app\views/layouts/icon.blade.php ENDPATH**/ ?>
